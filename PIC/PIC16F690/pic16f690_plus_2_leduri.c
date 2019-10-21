dc2c00089da5586750c8a89ec42bfa99562ebb00
DoruMandache <56799921+DoruMandache@users.noreply.github.com>
2019-10-21 18:28:42 +0300

    Create pic16f690_plus_2_leduri.c

---
+++ PIC/PIC16F690/pic16f690_plus_2_leduri.c
@@ -0,0 +1,19 @@
+#include "16F690.h" // header-ul pentru microcontroler
+
+#use delay(clock=4000000) // setarea frecvenţei de tact
+
+void main () // funcţia principală
+ {
+   while(1) // bucla fără sfârşit
+    {
+      output_high(PIN_C0); // aprinderea ledului conectat la pinul RC0 (trecerea în 1 logic)
+      output_low(PIN_C5); // ledul conectat la pinul RC5 stins (trecerea în 0 logic)
+      
+      delay_ms(1000); // întârziere de 1000 ms
+      
+      output_low(PIN_C0); // ledul conectat la pinul RC0 stins (trecerea în 0 logic)
+      output_high(PIN_C5); // aprinderea ledului conectat la pinul RC5 (trecerea în 1 logic)
+      
+      delay_ms(1000); // întârziere de 1000 ms
+    }
+ }
